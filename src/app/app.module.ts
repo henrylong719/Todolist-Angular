7c5c9aa59ab4235877e9211075c1bac9fbc85e0b
henrylong <henrylong719@gmail.com>
2021-03-31 14:38:40 +1030

    add bootstrap bootswatch fontawesome and header

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
